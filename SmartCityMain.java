e8cb4c86dba38b315540abf5aba4413ee9e6af48
Randika Lakshan <randika12335@gmail.com>
2025-10-21 20:46:38 +0530

    Main.java
    Randika Lakshan created SmartCityMain.java

---
+++ SmartCityMain.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class SmartCityMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Graph graph = new Graph();
+        LocationTree tree = new LocationTree();
+
+        while (true) {
+            System.out.println("\n--- Smart City Route Planner ---");
+            System.out.println("1. Add a new location");
+            System.out.println("2. Remove a location");
+            System.out.println("3. Add a road between locations");
+            System.out.println("4. Remove a road");
+            System.out.println("5. Display all connections");
+            System.out.println("6. Display all locations (AVL Tree)");
+            System.out.println("7. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1 -> {
+                    System.out.print("Enter location name: ");
+                    String loc = sc.nextLine();
+                    graph.addLocation(loc);
+                    tree.insert(loc);
+                }
+
+                case 2 -> {
+                    System.out.print("Enter location name to remove: ");
+                    graph.removeLocation(sc.nextLine());
+                }
+
+                case 3 -> {
+                    System.out.print("Enter first location: ");
+                    String from = sc.nextLine();
+                    System.out.print("Enter second location: ");
+                    String to = sc.nextLine();
+                    graph.addRoad(from, to);
+                }
+
+                case 4 -> {
+                    System.out.print("Enter first location: ");
+                    String from = sc.nextLine();
+                    System.out.print("Enter second location: ");
+                    String to = sc.nextLine();
+                    graph.removeRoad(from, to);
+                }
+
+                case 5 -> graph.displayConnections();
+
+                case 6 -> tree.display();
+
+                case 7 -> {
+                    System.out.println("Exiting... Thank you!");
+                    sc.close();
+                    System.exit(0);
+                }
+
+                default -> System.out.println("Invalid input! Try again.");
+            }
+        }
+    }
+}
